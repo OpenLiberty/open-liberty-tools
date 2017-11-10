681c34bbee5643a505c0bc87a1b1868a41d7585e
Keith Chong <kchong@ca.ibm.com>
2017-11-10 11:04:41 -0500

    [85] Gradle icon shows for Maven ghost runtime (#87)

--- dev/com.ibm.ws.st.liberty.gradle/src/com/ibm/ws/st/liberty/gradle/ui/rtexplorer/internal/GradleRuntimeProjectNode.java
+++ dev/com.ibm.ws.st.liberty.gradle/src/com/ibm/ws/st/liberty/gradle/ui/rtexplorer/internal/GradleRuntimeProjectNode.java
@@ -8,27 +8,28 @@
  * Contributors:
  * IBM Corporation - initial API and implementation
  *******************************************************************************/
-
 package com.ibm.ws.st.liberty.gradle.ui.rtexplorer.internal;
 
 import org.eclipse.core.resources.IProject;
 
-import com.ibm.ws.st.liberty.buildplugin.integration.internal.ILibertyBuildPluginImpl;
-import com.ibm.ws.st.liberty.buildplugin.integration.ui.rtexplorer.internal.AbstractLibertyBuildPluginRuntimeContentProvider;
 import com.ibm.ws.st.liberty.buildplugin.integration.ui.rtexplorer.internal.LibertyBuildPluginProjectNode;
-import com.ibm.ws.st.liberty.gradle.internal.LibertyGradle;
+import com.ibm.ws.st.liberty.gradle.internal.LibertyGradleConstants;
 
-public class GradleRuntimeContentProvider extends AbstractLibertyBuildPluginRuntimeContentProvider {
-	
-	@Override
-	public LibertyBuildPluginProjectNode createRuntimeProjectNode(IProject project, String text, String installDir, String description) {
-		return new GradleRuntimeProjectNode(project, text, installDir, description);
+public class GradleRuntimeProjectNode extends LibertyBuildPluginProjectNode {
+
+    /**
+     * @param project
+     * @param text
+     * @param installDir
+     * @param description
+     */
+	public GradleRuntimeProjectNode(IProject project, String text, String installDir, String description) {
+		super(project, text, installDir, description);
 	}
-	
+
     /** {@inheritDoc} */
     @Override
-    public ILibertyBuildPluginImpl getBuildPluginImpl() {
-        return LibertyGradle.getInstance();
+    public String getRuntimeContentId() {
+        return LibertyGradleConstants.LIBERTY_GRADLE_RUNTIME_CONTENT_ID;
     }
-
 }
